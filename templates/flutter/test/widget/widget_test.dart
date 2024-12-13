5d06cc352b1fafa330f3cef639f8c80fc35f15b0
ytsujita <ytsujita@cac.co.jp>
2024-12-13 20:46:49 +0900

    Fix test template

--- templates/flutter/test/widget/widget_test.dart
+++ templates/flutter/test/widget/widget_test.dart
@@ -1,14 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:{{ application_name }}/widget/page/{{ file_name }}';
+import 'package:{{ application_name }}/{{ file_name }}';
 
 void main() {
   testWidgets(
     '{{ widget_name|pascal }}WidgetTest',
     (WidgetTester tester) async {
       await tester.pumpWidget(
-        const {{ widget_name|pascal}}(),
+        const {{ widget_name|pascal }}Widget(),
       );
     },
   );
